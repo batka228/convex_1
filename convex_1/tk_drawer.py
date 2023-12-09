b813e1388b438d8caaa7fb2ae3570074c64f3e2f
batka228 <117315589+batka228@users.noreply.github.com>
2023-12-10 00:52:04 +0300

    Update tk_drawer.py

--- convex_1/tk_drawer.py
+++ convex_1/tk_drawer.py
@@ -8,17 +8,17 @@ SCALE = 50
 
 
 def x(p):
-    """ преобразование x-координаты """
+    """преобразование x-координаты"""
     return SIZE / 2 + SCALE * p.x
 
 
 def y(p):
-    """ преобразование y-координаты """
+    """преобразование y-координаты"""
     return SIZE / 2 - SCALE * p.y
 
 
 class TkDrawer:
-    """ Графический интерфейс для выпуклой оболочки """
+    """Графический интерфейс для выпуклой оболочки"""
 
     # Конструктор
     def __init__(self):
@@ -26,7 +26,7 @@ class TkDrawer:
         self.root.title("Выпуклая оболочка")
         self.root.geometry(f"{SIZE+5}x{SIZE+5}")
         self.root.resizable(False, False)
-        self.root.bind('<Control-c>', quit)
+        self.root.bind("<Control-c>", quit)
         self.canvas = Canvas(self.root, width=SIZE, height=SIZE)
         self.canvas.pack(padx=5, pady=5)
 
@@ -43,8 +43,10 @@ class TkDrawer:
 
     # Рисование точки
     def draw_point(self, p):
-        self.canvas.create_oval(
-            x(p) + 1, y(p) + 1, x(p) - 1, y(p) - 1, fill="black")
+        self.canvas.create_oval(x(p) + 1,
+                                y(p) + 1,
+                                x(p) - 1,
+                                y(p) - 1, fill="black")
         self.root.update()
 
     # Рисование линии
@@ -54,15 +56,27 @@ class TkDrawer:
 
     def draw_oval(self, p):
         self.canvas.create_oval(
-            x(p) + 50, y(p) + 50, x(p) - 50, y(p) - 50, fill="black")
+            x(p) + 50, y(p) + 50, x(p) - 50, y(p) - 50, fill="black"
+        )
         self.canvas.create_oval(
-            x(p) + 49, y(p) + 49, x(p) - 49, y(p) - 49, fill="white")
-        p_1, p_2 = R2Point(1.0, 0.0), R2Point(-1.0, 0.0)
-        x1, y1, x2, y2 = x(p_1), y(p_1), x(p_2), y(p_2)
-        self.canvas.create_line(x1, y1, x2, y2, fill="blue", width=1)
-        p_1, p_2 = R2Point(0.0, 1.0), R2Point(0.0, -1.0)
-        x1, y1, x2, y2 = x(p_1), y(p_1), x(p_2), y(p_2)
-        self.canvas.create_line(x1, y1, x2, y2, fill="blue", width=1)
+            x(p) + 49, y(p) + 49, x(p) - 49, y(p) - 49, fill="white"
+        )
+        self.canvas.create_line(
+            x(R2Point(1.0, 0.0)),
+            y(R2Point(1.0, 0.0)),
+            x(R2Point(-1.0, 0.0)),
+            y(R2Point(-1.0, 0.0)),
+            fill="blue",
+            width=1,
+        )
+        self.canvas.create_line(
+            x(R2Point(0.0, 1.0)),
+            y(R2Point(0.0, 1.0)),
+            x(R2Point(0.0, -1.0)),
+            y(R2Point(0.0, -1.0)),
+            fill="blue",
+            width=1,
+        )
         self.root.update()
 
 
@@ -70,6 +84,7 @@ if __name__ == "__main__":
 
     import time
     from r2point import R2Point
+
     tk = TkDrawer()
     tk.clean()
     # tk.draw_point(R2Point(2.0, 2.0))
